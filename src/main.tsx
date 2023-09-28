cab20f48655277871cc005a0f6fd875d443f4ee5
june <ryu9663@naver.com>
2023-09-28 17:24:20 +0900

    fix : router구조 및 header 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+
 import './index.css';
 import 'junyeol-components/style.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home/index.tsx';
+import Carrer from './pages/Carrer/index.tsx';
+import WhatIDid from './pages/WhatIDid/index.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    children: [
+      {
+        path: '/carrer',
+        element: <Carrer />,
+      },
+      {
+        path: '/whatidid',
+        element: <WhatIDid />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,33 @@
-import { Button } from 'junyeol-components';
+import { Button, Header } from 'junyeol-components';
+import { Link, Outlet } from 'react-router-dom';
 
 const Home = () => {
   const handleClickButton = () => console.log('click');
   return (
-    <div>
-      <Button onClick={handleClickButton}>button</Button>
-      Home
-      <button>hi</button>
-    </div>
+    <>
+      <main>
+        <Header>
+          {[
+            { link: '/', name: '자기소개' },
+            { link: '/carrer', name: '이력' },
+            { link: '/whatidid', name: '했던 일' },
+          ].map((button, i) => (
+            <li key={i}>
+              <Link to={button.link}>
+                <Button>{button.name}</Button>
+              </Link>
+            </li>
+          ))}
+        </Header>
+        <a href="/">home</a>
+        <Button onClick={handleClickButton}>button</Button>
+        Home
+        <button>hi</button>
+        <div id="detail">
+          <Outlet />
+        </div>
+      </main>
+    </>
   );
 };
 
