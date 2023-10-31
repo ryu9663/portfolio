03d00bcb4ac0c99714dac51fa46c994083587760
june <ryu9663@naver.com>
2023-10-31 16:32:18 +0900

    feat: PR-6 드래그앤드롭 기능

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,28 +1,4 @@
-import Draggable from 'react-draggable';
-import styles from './index.module.scss';
-import { Icon } from '@/pages/Home/components/Icon';
-import { useState } from 'react';
-import { ICON_IMAGES } from '@/utils/constant';
+import { Draggable } from '@/pages/Home/components/Draggable';
+import { ICONS } from '@/utils/constant';
 
-const Home = () => {
-  const [isReadOnly, setIsReadOnly] = useState(true);
-  return (
-    <div className={styles.home} onClick={() => !isReadOnly && setIsReadOnly(true)}>
-      <Draggable>
-        <div
-          onClick={e => {
-            !isReadOnly && e.stopPropagation();
-          }}
-        >
-          {ICON_IMAGES.map((iconImg, i) => (
-            <div key={i}>
-              <Icon isReadOnly={isReadOnly} setIsReadOnly={setIsReadOnly} iconImg={iconImg} />
-            </div>
-          ))}
-        </div>
-      </Draggable>
-    </div>
-  );
-};
-
-export default Home;
+export const Home = () => <Draggable icons={ICONS} />;
--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,3 +1,7 @@
 import NotionSVG from '@/assets/notion.svg';
+import GithubSVG from '@/assets/github.svg';
 
-export const ICON_IMAGES = [{ src: NotionSVG, alt: 'notion' }];
+export const ICONS = [
+  { id: 1, src: NotionSVG, alt: 'notion', left: 50, top: 50 },
+  { id: 2, src: GithubSVG, alt: 'github', left: 150, top: 50 },
+];
