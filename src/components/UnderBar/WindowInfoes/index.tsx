1a5a3c7fb1d96ac312c8418a358e01c1e55cef45
june <ryu9663@naver.com>
2023-11-06 11:59:59 +0900

    windowInfoes 스타일을 underbar에서 분리

--- src/components/UnderBar/WindowInfoes/index.tsx
+++ src/components/UnderBar/WindowInfoes/index.tsx
@@ -1,14 +1,14 @@
 import { useUnderbarStore } from '@/components/UnderBar/index.store';
-import styles from '../index.module.scss';
+import styles from './index.module.scss';
 import { Button } from 'junyeol-components';
 
 export const WindowInfoes = () => {
   const iconsOnUnderbar = useUnderbarStore(state => state.iconsOnUnderbar);
   return (
-    <ul className={styles['underbar_left_window-info']}>
+    <ul className={styles['window_infoes']}>
       {iconsOnUnderbar.map(icon => (
         <li data-testid="windowinfo" key={icon.id}>
-          <Button className={styles['underbar_left_window-info-button']}>
+          <Button className={styles['window_infoes-button']}>
             <img src={icon.src} alt={icon.alt} width={30} height={30} /> <span>{icon.alt}</span>
           </Button>
         </li>
