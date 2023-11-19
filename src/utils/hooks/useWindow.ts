ded219beedc9c6fce8aa10d5daf7dc0f6f4dcee9
june <66232436+ryu9663@users.noreply.github.com>
2023-11-19 22:03:13 +0900

    feat: PR-35 시작버튼 통해 파일 열 수 있도록 하기 (#32)

---
+++ src/utils/hooks/useWindow.ts
@@ -0,0 +1,29 @@
+import { IconType } from '@/components/Icon';
+import { useUnderbarStore } from '@/components/UnderBar/index.store';
+import { useWindowBoxStore } from '@/components/WindowBox/index.store';
+import { getZIndexesWithChildren } from '@/utils';
+import { useThisWindowState } from '@/utils/hooks/useThisWindow';
+
+export const useWindow = (icon: IconType) => {
+  const [windows] = useWindowBoxStore(state => [state.windows]);
+  const [iconsOnUnderbar, setIconsOnUnderbar] = useUnderbarStore(state => [
+    state.iconsOnUnderbar,
+    state.setIconsOnUnderbar,
+  ]);
+
+  const setThisWindowState = useThisWindowState(icon.id, windows);
+
+  const openWindow = (icon: IconType) => {
+    const zIndexs = getZIndexesWithChildren(windows);
+    if (icon.type === IconType.FOLDER || icon.type === IconType.FILE) {
+      setThisWindowState(
+        { ...icon, windowState: 'normal', activated: true, zIndex: Math.max(...zIndexs) + 1 },
+        { activated: false },
+      );
+      setIconsOnUnderbar([...iconsOnUnderbar, icon]);
+    } else if (icon.type === IconType.LINK) {
+      window.open(icon.link);
+    }
+  };
+  return openWindow;
+};
