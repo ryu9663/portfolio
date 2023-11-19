1d6fdceeea9195491dcecaceaa6d9797a8752662
june <ryu9663@naver.com>
2023-11-19 18:02:44 +0900

    feat:PR-33 아이콘 이름 너무 길면 ellipsis로 보이고 호버했을때 전체 다 보이도록 변경

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,6 +1,7 @@
 import NotionSVG from '@/assets/notion.svg';
 import GithubSVG from '@/assets/github.svg';
 import FolderSVG from '@/assets/folder.svg';
+import CodestatesLogo from '@/assets/codestates-logo.svg';
 import { IconType } from '@/components/Icon';
 
 export const ICONS: IconType[] = [
@@ -8,7 +9,7 @@ export const ICONS: IconType[] = [
     type: 'link',
     id: 1,
     src: NotionSVG,
-    alt: 'notion',
+    alt: '이력서',
     left: 50,
     top: 50,
     link: 'https://www.notion.so/41daf0a85b9f46d186114573d2781a78?pvs=4',
@@ -17,7 +18,7 @@ export const ICONS: IconType[] = [
     type: 'link',
     id: 2,
     src: GithubSVG,
-    alt: 'github',
+    alt: '깃허브',
     left: 150,
     top: 50,
     link: 'https://github.com/ryu9663',
@@ -27,7 +28,7 @@ export const ICONS: IconType[] = [
     windowState: 'closed',
     id: 3,
     src: FolderSVG,
-    alt: 'Project',
+    alt: '프로젝트',
     left: 250,
     top: 50,
     zIndex: 100,
@@ -36,8 +37,8 @@ export const ICONS: IconType[] = [
         type: 'file',
         windowState: 'closed',
         id: 4,
-        src: NotionSVG,
-        alt: 'notion',
+        src: CodestatesLogo,
+        alt: '코드스테이츠 랜딩페이지',
         left: 50,
         top: 50,
         zIndex: 100,
