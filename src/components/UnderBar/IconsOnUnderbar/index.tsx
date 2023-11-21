bb5e0c7e3d47157f2b4db2414cb57a0e0705c577
june <66232436+ryu9663@users.noreply.github.com>
2023-11-21 21:14:47 +0900

    hotfix: PR-41 이름바꾸기 동기화 안되는 이슈 , 최소화 했을때 애니메이션 이상한 것 (#36)
    * fix: 최소화 이상하게 되는 것
    
    * fix: 이름바꾸기 동기화 안되는 문제

--- src/components/UnderBar/IconsOnUnderbar/index.tsx
+++ src/components/UnderBar/IconsOnUnderbar/index.tsx
@@ -88,7 +88,7 @@ const IconOnUnderbar = ({ icon }: { icon: Pick<OpenableIconType, 'id'> }) => {
   })();
 
   return (
-    <li data-testid="windowinfo" key={thisWindow.id}>
+    <li data-testid="windowinfo" key={icon.id}>
       <Button
         className={styles['window_infoes-button']}
         onContextMenu={e => {
